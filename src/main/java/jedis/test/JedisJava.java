dd2ad2953892adc654bca9f46c895b4978de58d8
ex-caoqf001 <ex-caoqf001@cmrh.com>
2018-07-17 09:08:50 +0800

    提交内容：初始提交，jedis链接Redis.

---
+++ src/main/java/jedis/test/JedisJava.java
@@ -0,0 +1,40 @@
+package jedis.test;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import redis.clients.jedis.JedisPoolConfig;
+import redis.clients.jedis.JedisShardInfo;
+import redis.clients.jedis.ShardedJedis;
+import redis.clients.jedis.ShardedJedisPool;
+@EnableAsync
+@EnableScheduling
+@EnableTransactionManagement
+public class JedisJava {
+    private static ShardedJedisPool pool;
+    static {
+        JedisPoolConfig config = new JedisPoolConfig();
+        config.setMaxTotal(100);
+        config.setMaxIdle(50);
+        config.setMaxWaitMillis(3000);
+        config.setTestOnBorrow(true);
+        config.setTestOnReturn(true);
+        // 集群
+        JedisShardInfo jedisShardInfo1 = new JedisShardInfo("127.0.0.1", 6379);
+        jedisShardInfo1.setPassword("test");
+        List<JedisShardInfo> list = new LinkedList<JedisShardInfo>();
+        list.add(jedisShardInfo1);
+        pool = new ShardedJedisPool(config, list);
+    }
+    public static void main(String[] args) {
+        ShardedJedis jedis = pool.getResource();
+        String keys = "myname";
+        jedis.set(keys, "lxr");
+        String result = jedis.get(keys);
+        System.out.println(result);
+    }
+}
